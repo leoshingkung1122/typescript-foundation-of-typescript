bc9e133708d6cc92b01a526e64cf6641183ff46a
Phumphut <phumphut.rueng@gmail.com>
2025-07-30 15:20:09 +0700

    feat: answer ex1-4.2

--- src/ex-2.ts
+++ src/ex-2.ts
@@ -1,6 +1,5 @@
 function sumEvenNumbers(numbers: number[]): number {
-  //Start Coding Here
+  return numbers.filter((evenNumbers)=> evenNumbers%2 == 0).reduce((acc,curr) => acc +curr,0)
 }
-
 console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6])); // 12
 console.log(sumEvenNumbers([11, 13, 15])); // 0
